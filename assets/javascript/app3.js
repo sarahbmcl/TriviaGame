b47625c7e27601fd6c950e72a6a3bff6ac3718d3
Sarah McLaughlin <sarahbmcl@gmail.com>
2019-08-23 23:10:09 -0500

    basic logic added

--- assets/javascript/app3.js
+++ assets/javascript/app3.js
@@ -32,16 +32,42 @@ const quizQuestions = [
 ]
 
 //Initial values
-let counter = 30; //seconds to start
+let counter = 30;
 let currentQuestion = 0; //pulls question from the array
 let score = 0; //correctly guessed
 let lost = 0; //incorrectly guessed
-let unanswered = 0; //no response
+let unanswered = 0; 
 let timer; //holds id of set interval of clock
+
+function nextQuestion() {
+    const isQuestionOver = (quizQuestions.length - 1) === currentQuestion;
+    if (isQuestionOver){
+        console.log("Game is over")
+    }else{
+    currentQuestion++;
+    loadQuestion();
+    }
+}
+
+function timeUp() {
+    clearInterval(timer);
+    unanswered++;
+    nextQuestion();
+}
+
+function countDown() {
+    counter--;
+    $("#time").html("Timer: " + counter); //**** */
+    if (counter === 0) {
+        timeUp();
+    }
+}
 
 //display the question and choices together in the browser
 function loadQuestion() {
-
+    counter = 30; //always call function with 30 initial value
+    timer = setInterval(countDown, 1000);//*** */
+    
     //call on current question with answer choices from array
     const question = quizQuestions [currentQuestion].question;
     const choices = quizQuestions [currentQuestion].choices;
@@ -66,5 +92,25 @@ function loadChoices(choices) {
 
     return result; 
 }
+
+//**** more functional than $(".choice").on("click", function()
+$(document).on("click", ".choice", function() {
+    clearInterval(timer);
+    const selectedAnswer = $(this).attr('data-answer');
+    const correctAnswer = quizQuestions [currentQuestion].correctAnswer;
+
+    if (correctAnswer === selectedAnswer){
+        score++; 
+        console.log ("correct");
+        nextQuestion();
+    }else{
+        lost++;
+        console.log ("incorrect");
+        nextQuestion();
+    }
+    
+
+    console.log ("test answer", selectedAnswer)
+});
 
 loadQuestion();
